2981ed9612acdfbcf742a0c12abc52d44413aa30
Omar Tejada <tejada.omar03@gmail.com>
2021-06-18 22:59:48 -0600

    Add awesome

---
+++ .config/awesome/themes/Morning/theme.lua
@@ -0,0 +1,55 @@
+local theme = {}
+
+theme.font          = 'JetBrains Mono 10'
+
+theme.bg_normal     = "#37395E"
+theme.bg_focus      = "#4C4f82"
+theme.bg_urgent     = "#48FFDC"
+theme.bg_minimize   = "#1B1D27"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#ECBDF6"
+theme.fg_focus      = "#F2E1F6"
+theme.fg_urgent     = "#F2E1F6"
+theme.fg_minimize   = "#F2E1F6"
+
+theme.useless_gap   = 5
+theme.border_width  = 3
+theme.border_normal = "#D35DFF"
+theme.border_focus  = "#4BFFDC"
+theme.border_marked = "#D000FF"
+
+-- Hotkeys popup
+theme.hotkeys_bg = '#37395E'
+theme.hotkeys_fg = '#ECBDF6'
+theme.hotkeys_opacity = '0.9'
+theme.hotkeys_border_width = 3
+theme.hotkeys_border_color = '#48FFDC'
+theme.hotkeys_modifiers_fg = '#1DFF79'
+theme.hotkeys_label_fg = '#1B1D27'
+theme.hotkeys_font = 'JetBrains Mono 10'
+theme.hotkeys_description_font = 'Fira Code 10'
+theme.hotkeys_group_margin = 20
+
+-- Uncomment to enable pictures for layout
+-- theme.layout_tile       = "~/.config/awesome/themes/zenburn/layouts/tile.png"
+-- theme.layout_tileleft   = "~/.config/awesome/themes/zenburn/layouts/tileleft.png"
+-- theme.layout_tilebottom = "~/.config/awesome/themes/zenburn/layouts/tilebottom.png"
+-- theme.layout_tiletop    = "~/.config/awesome/themes/zenburn/layouts/tiletop.png"
+-- theme.layout_fairv      = "~/.config/awesome/themes/zenburn/layouts/fairv.png"
+-- theme.layout_fairh      = "~/.config/awesome/themes/zenburn/layouts/fairh.png"
+-- theme.layout_spiral     = "~/.config/awesome/themes/zenburn/layouts/spiral.png"
+-- theme.layout_dwindle    = "~/.config/awesome/themes/zenburn/layouts/dwindle.png"
+-- theme.layout_max        = "~/.config/awesome/themes/zenburn/layouts/max.png"
+-- theme.layout_fullscreen = "~/.config/awesome/themes/zenburn/layouts/fullscreen.png"
+-- theme.layout_magnifier  = "~/.config/awesome/themes/zenburn/layouts/magnifier.png"
+-- theme.layout_floating   = "~/.config/awesome/themes/zenburn/layouts/floating.png"
+-- theme.layout_cornernw   = "~/.config/awesome/themes/zenburn/layouts/cornernw.png"
+-- theme.layout_cornerne   = "~/.config/awesome/themes/zenburn/layouts/cornerne.png"
+-- theme.layout_cornersw   = "~/.config/awesome/themes/zenburn/layouts/cornersw.png"
+-- theme.layout_cornerse   = "~/.config/awesome/themes/zenburn/layouts/cornerse.png"
+
+-- Wallpaper
+theme.wallpaper = '~/.config/awesome/themes/Morning/wallpaper.jpg'
+
+return theme
