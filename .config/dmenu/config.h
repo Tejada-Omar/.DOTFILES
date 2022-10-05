5b7a4ea5167aee0a156cc4d5fc305929dc01b2ca
Omar Tejada <tejada.omar03@gmail.com>
2022-10-05 07:57:04 -0600

    Add dmenu config files
    config.h is hardlinked while patches/ is just copied

---
+++ .config/dmenu/config.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+   "Fira Code:pixelsize=13"
+};
+static const char *prompt      = "run: ";      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#1a1b26" },
+	[SchemeSel] = { "#ffffff", "#3d59a1" },
+	[SchemeSelHighlight] = { "#c0caf5", "#3d59a1" },
+	[SchemeNormHighlight] = { "#c0caf5", "#1a1b26" },
+	[SchemeOut] = { "#414868", "#c0caf5" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 26;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
