89a0e84584e65c0f0c04dd9c7ea8bffdd4fc368b
Omar Tejada <tejada.omar03@gmail.com>
2022-12-29 04:03:29 -0700

    nvim: update config for packer bootstrapping

--- .config/nvim/lua/omar/plugins.lua
+++ .config/nvim/lua/omar/plugins.lua
@@ -1,5 +1,19 @@
-vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
+  use('wbthomason/packer.nvim')
+
   use('nanotee/luv-vimdocs')
   use('milisims/nvim-luaref')
   use('nvim-lua/plenary.nvim')
@@ -237,9 +251,14 @@ return require('packer').startup(function(use)
 
   -- Local plugins
   use {
-    '~/Code/nvim-plugins/notes-compile',
+    -- '~/Code/nvim-plugins/notes-compile',
+    'Tejada-Omar/notes-compile.nvim',
     ft = { 'markdown', 'vimwiki' },
     requires = { 'nvim-lua/plenary.nvim' },
     config = function() require('notes-compile').setup() end,
   }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
