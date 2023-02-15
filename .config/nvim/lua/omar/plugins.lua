23071712c11719e5f939a814359ad248b2159903
Omar Tejada <tejada.omar03@gmail.com>
2023-02-15 10:50:24 -0700

    nvim: bootstrap packer

--- .config/nvim/lua/omar/plugins.lua
+++ .config/nvim/lua/omar/plugins.lua
@@ -1,5 +1,27 @@
-vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')
+      .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system {
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path,
+    }
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
+  use('wbthomason/packer.nvim')
+
   use('nanotee/luv-vimdocs')
   use('milisims/nvim-luaref')
   use('nvim-lua/plenary.nvim')
@@ -29,6 +51,7 @@ return require('packer').startup(function(use)
       after = 'nvim-treesitter',
     },
     { 'nvim-treesitter/nvim-treesitter-context', after = 'nvim-treesitter' },
+    { 'nvim-treesitter/playground', after = 'nvim-treesitter' },
   }
 
   use {
@@ -238,7 +261,7 @@ return require('packer').startup(function(use)
 
   use {
     'nvim-neorg/neorg',
-    tag = '*',
+    -- tag = '*',
     -- run = ':Neorg sync-parsers',
     after = 'nvim-treesitter',
     requires = {
