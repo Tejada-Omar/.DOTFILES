425e0067d6e7acdc7c370501552bb149ead269d8
Omar Tejada <tejada.omar03@gmail.com>
2023-05-25 00:51:43 -0600

    Remove lazy in preparation for submodule migration

--- .config/nvim/lua/omar/lazy/notes-compile.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'Tejada-Omar/notes-compile.nvim',
-    dev = false,
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    ft = { 'markdown', 'vimwiki' },
-    config = true,
-  },
-}
