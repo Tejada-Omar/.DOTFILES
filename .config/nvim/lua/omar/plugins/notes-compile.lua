aa83a2be2bbfd457d97b18c9197e282275030fcd
Omar Tejada <tejada.omar03@gmail.com>
2023-12-28 23:03:41 -0700

    nvim: update notes-compile config

--- .config/nvim/lua/omar/plugins/notes-compile.lua
+++ .config/nvim/lua/omar/plugins/notes-compile.lua
@@ -1,3 +1,7 @@
 require('notes-compile').setup {
-  pandoc_integration = true,
+  -- pandoc_args = {
+  --   { '-f', 'commonmark_x+rebase_relative_paths+implicit_figures+sourcepos' },
+  -- },
+  zathura_integration = true,
+  -- synctex_integration = true,
 }
