29d81a2fefee00d55086ae180df9ae9a12dcd61a
Omar Tejada <tejada.omar03@gmail.com>
2024-05-24 21:34:56 -0600

    feat(nvim): swap zen mode plugins
    Folke plugin is better maintained

--- .config/nvim/lua/omar/plugins/theme.lua
+++ .config/nvim/lua/omar/plugins/theme.lua
@@ -6,9 +6,7 @@ return {
   {
     'folke/tokyonight.nvim',
     priority = 1000,
-    init = function ()
-      vim.cmd([[colorscheme tokyonight-night]])
-    end,
+    init = function() vim.cmd([[colorscheme tokyonight-night]]) end,
     opts = function()
       vim.api.nvim_create_autocmd('ColorScheme', {
         pattern = '*',
@@ -73,48 +71,26 @@ return {
   },
   {
     {
-      'Pocco81/true-zen.nvim',
+      'folke/zen-mode.nvim',
       opts = {
-        integrations = {
-          twilight = true,
-          lualine = true,
+        window = {
+          backdrop = 1,
+          options = {
+            list = true,
+          },
+        },
+        plugins = {
+          options = {
+            ruler = true,
+          },
+          twilight = { enabled = true },
+          gitsigns = { enabled = true },
+          tmux = { enabled = true },
         },
       },
-      config = function(_, opts)
-        local truezen = require('true-zen')
-
-        require('true-zen').setup(opts)
-
-        vim.keymap.set('n', '<leader>zn', function()
-          local last = vim.api.nvim_buf_line_count(0)
-          truezen.narrow(0, last)
-        end, { desc = 'true-zen narrow' })
-
-        vim.keymap.set('v', '<leader>zn', function()
-          local first = vim.fn.line('v')
-          local last = vim.fn.line('.')
-          truezen.narrow(first, last)
-        end, { desc = 'true-zen narrow' })
-
-        vim.keymap.set(
-          'n',
-          '<leader>zf',
-          truezen.focus,
-          { desc = 'true-zen focus' }
-        )
-        vim.keymap.set(
-          'n',
-          '<leader>zm',
-          truezen.minimalist,
-          { desc = 'true-zen minimalist' }
-        )
-        vim.keymap.set(
-          'n',
-          '<leader>za',
-          truezen.ataraxis,
-          { desc = 'true-zen ataraxis' }
-        )
-      end,
+      keys = {
+        { '<leader>z', '<CMD>ZenMode<CR>', desc = 'Toggle zen mode' },
+      },
     },
     {
       'folke/twilight.nvim',
