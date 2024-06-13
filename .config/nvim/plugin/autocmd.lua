dcd9de7ea6c08441131deac03f126a8786913f8d
Omar Tejada <tejada.omar03@gmail.com>
2024-06-13 00:36:17 -0600

    fix(nvim): better hl group choice on yank

--- .config/nvim/plugin/autocmd.lua
+++ .config/nvim/plugin/autocmd.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function() vim.highlight.on_yank { higroup = 'Visual' } end,
+  callback = function() vim.highlight.on_yank { higroup = 'IncSearch' } end,
 })
 
 vim.api.nvim_create_autocmd({ 'LspAttach', 'InsertEnter', 'InsertLeave' }, {
