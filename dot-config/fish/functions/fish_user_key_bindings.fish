9aa646c5cd0027dd3f3c6cfc460b406587b787f2
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-06-04 04:33:48 -0600

    feat(fish): cdd-like binds in cmdline
    Implemented using __fzf_shortcut

--- dot-config/fish/functions/fish_user_key_bindings.fish
+++ dot-config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,12 @@
 function fish_user_key_bindings
+  # Standard binds
   fish_default_key_bindings -M insert
   fish_vi_key_bindings --no-erase
   bind -M insert -m default jk repaint-mode
+
+  # Fzf shortcuts
+  bind -M insert alt-f 'commandline -i (__fzf_shortcut -tf -r .)'
+  bind -M insert alt-shift-f 'commandline -i (__fzf_shortcut -tf -i -r .)'
+  bind -M insert alt-d 'commandline -i (__fzf_shortcut -td -r .)'
+  bind -M insert alt-shift-d 'commandline -i (__fzf_shortcut -td -i -r .)'
 end
