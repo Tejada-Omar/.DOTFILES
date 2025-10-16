5a383e4503d171fd2be8daf78895e71b8f1b00b1
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-10-16 01:49:12 -0600

    feat(fish): add neovim profile envvar

--- dot-config/fish/conf.d/env.fish
+++ dot-config/fish/conf.d/env.fish
@@ -25,5 +25,6 @@ set -gx NTFY_TOPIC "ping"
 set -gx NTFY_TAGS "$hostname"
 
 # Enable LSP support for nvim config
+set -gx NVIM_PROFILE "desktop"
 set -gx NVIM_LSP_ENABLED "true"
 set -gx NVIM_AUTOFORMAT_ENABLED "true"
