82074a72d172c5c1a94f1ed7ceadc77272c7c77f
Omar Tejada <72728094+Tejada-Omar@users.noreply.github.com>
2025-11-08 21:32:58 -0700

    refactor(env): move envvars from shell to systemd

--- dot-config/fish/conf.d/env.fish
+++ dot-config/fish/conf.d/env.fish
@@ -1,17 +1,10 @@
 export (envsubst < $HOME/.config/environment.d/env.conf)
 
 set -gx PAGER bat
-set -gx READER "zathura"
-set -gx BROWSER "firefox"
-set -gx GOPATH "$HOME/go"
-set -gx USB_AUTOSUSPEND 1
 
 fish_add_path -g \
-  "$HOME/.local/bin" \
   "$XDG_DATA_HOME/nvim/mason/bin" \
   "$XDG_CONFIG_HOME/emacs/bin" \
-  "$GOPATH/bin" \
-  "$HOME/.cargo/bin"
 
 # Use bat as a colorizing pager for man
 set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
